546c6f176e0ac700d1b78ac1887ed05721ab4955
Chookity96 <jegathees35@gmail.com>
2022-06-26 20:06:49 +0800

    bug fix

--- pages/fleet/bikes/[id].js
+++ pages/fleet/bikes/[id].js
@@ -37,64 +37,66 @@ function Bike() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Nav className="w-full" />
-      <div className="w-full mx-auto space-y-4">
-        <div className="flex flex-col-reverse justify-between bg-white p-8 md:flex-row lg:px-20 relative">
-          <button
-            className="absolute top-3 left-7 md:p-3 xl:pl-11"
-            onClick={() => navigate.push('/fleet')}
-          >
-            <ChevronLeftIcon className="h-6" />
-          </button>
-          <img src={bike?.image} alt="" />
-          <div className="">
-            <h1 className="text-2xl font-semibold mt-2">Our Fleet</h1>
-            <h2 className="text-4xl sm:text-5xl font-bold mb-5">
-              {bike?.name}
-            </h2>
-            <button className="border-2 border-black hover:text-[#fd2084] hover:border-[#fd2084] transition-all ease-linear cursor-pointer py-2 px-4 text-xl rounded-md font-bold max-w-[240px]">
-              Book now
+      <div className='absolute bg-white top-24 lg:top-20 left-0 w-full h-[394px]'></div>
+        <div className="w-full mx-auto space-y-4 max-w-6xl">
+          <div className="flex flex-col-reverse justify-between p-5 md:flex-row relative bg-white">
+            {/* <div className='absolute bg-white top-0 left-[-190px] w-screen h-28 z-[0'></div> */}
+            <button
+              className="absolute top-3 left-3"
+              onClick={() => navigate.push('/fleet')}
+            >
+              <ChevronLeftIcon className="h-6" />
             </button>
-          </div>
-        </div>
-        <div className="p-5 space-y-16 max-w-6xl">
-          <div className="space-y-5">
-            <h1 className="text-3xl sm:text-4xl font-bold text-[#19f9b9]">
-              About the bike
-            </h1>
-            <div className="flex space-x-10">
-              <div>
-                <h3 className="text-md sm:text-lg text-gray-500 uppercase">
-                  Fuel
-                </h3>
-                <p className="text-white text-xl sm:text-2xl font-semibold">
-                  Petrol
-                </p>
-              </div>
-
-              <div>
-                <h3 className="text-md sm:text-lg text-gray-500 uppercase">
-                  Description
-                </h3>
-                <p className="text-white text-xl sm:text-2xl font-semibold">
-                  {bike?.about}
-                </p>
-              </div>
+            <img src={bike?.image} alt="" />
+            <div className="">
+              <h1 className="text-2xl font-semibold mt-2">Our Fleet</h1>
+              <h2 className="text-4xl sm:text-5xl font-bold mb-5">
+                {bike?.name}
+              </h2>
+              <button className="border-2 border-black hover:text-[#fd2084] hover:border-[#fd2084] transition-all ease-linear cursor-pointer py-2 px-4 text-xl rounded-md font-bold max-w-[240px]">
+                Book now
+              </button>
             </div>
           </div>
+          <div className="p-5 space-y-16 max-w-6xl">
+            <div className="space-y-5">
+              <h1 className="text-3xl sm:text-4xl font-bold text-[#19f9b9]">
+                About the bike
+              </h1>
+              <div className="flex space-x-10">
+                <div>
+                  <h3 className="text-md sm:text-lg text-gray-500 uppercase">
+                    Fuel
+                  </h3>
+                  <p className="text-white text-xl sm:text-2xl font-semibold">
+                    Petrol
+                  </p>
+                </div>
 
-          <div className="">
-            <h1 className="text-3xl sm:text-4xl  font-bold text-[#19f9b9] mb-2">
-              User guide
-            </h1>
-            <p className="text-white text-xl sm:text-2xl mb-10">
-              Learn more about this bike
-            </p>
-            <button className="border-2 border-[#19f9b9] bg-[#19f9b9] cursor-pointer py-2 px-4 text-xl rounded-md font-semibold max-w-[240px]">
-              Learn more
-            </button>
+                <div>
+                  <h3 className="text-md sm:text-lg text-gray-500 uppercase">
+                    Description
+                  </h3>
+                  <p className="text-white text-xl sm:text-2xl font-semibold">
+                    {bike?.about}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="">
+              <h1 className="text-3xl sm:text-4xl  font-bold text-[#19f9b9] mb-2">
+                User guide
+              </h1>
+              <p className="text-white text-xl sm:text-2xl mb-10">
+                Learn more about this bike
+              </p>
+              <button className="border-2 border-[#19f9b9] bg-[#19f9b9] cursor-pointer py-2 px-4 text-xl rounded-md font-semibold max-w-[240px]">
+                Learn more
+              </button>
+            </div>
           </div>
         </div>
-      </div>
       <Footer />
     </div>
   )
